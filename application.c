07908dbc7362710a7a29b844bf91d80ed41280a9
radicdjolee <114096761+radicdjolee@users.noreply.github.com>
2023-02-13 12:17:17 +0100

    Add files via upload

--- application.c
+++ application.c
@@ -30,7 +30,10 @@ int main(void){
 	}
 	
 	fread(buffer_bram_a, sizeof(char), numbytes, textfile);
-	fclose(textfile);
+	if(fclose(textfile)){
+		printf("Problem pri zatvaranju text_file bram_a.txt\n");
+		return -1;	
+	}
 	printf("********************************\n");
 	printf("*************BRAM_A*************\n");
 	printf(buffer_bram_a);
@@ -54,7 +57,12 @@ int main(void){
 	}
 	
 	fread(buffer_bram_b, sizeof(char), numbytes, textfile);
-	fclose(textfile);
+
+	if(fclose(textfile)){
+		printf("Problem pri zatvaranju text_file bram_b.txt\n");
+		return -1;	
+	}
+
 	printf("********************************\n");
 	printf("*************BRAM_B*************\n");
 	printf(buffer_bram_b);
@@ -66,18 +74,15 @@ int main(void){
 	fp = fopen("/dev/bram_a", "w");
 	
 	if(fp==NULL){
-		
 		printf("Nije moguce otvoriti /dev/bram_a.\n");
-		return -1;
-		
+		return -1;	
 	}
 
 	fputs(buffer_bram_a,fp);
-	if(fclose(fp)){
-		
+
+	if(fclose(fp)){	
 		printf("Problem pri zatvaranju /dev/bram_a.\n");
 		return -1;	
-		
 	}
 
 
