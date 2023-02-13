07ffb8a042ece4a6c931a6c47862cb047619bc1d
radicdjolee <114096761+radicdjolee@users.noreply.github.com>
2023-02-13 11:55:01 +0100

    Add files via upload

--- application.c
+++ application.c
@@ -7,25 +7,34 @@
 int main(void){
 
     FILE *fp;
-    char *buffer_bram_a = NULL;
+	FILE *textfile;
+    char *buffer_bram_a;
     char *buffer_bram_b = NULL;
 	int  *bram_a_txt_array, *bram_b_txt_array;
-    size_t size = 0;
+    long numbytes;
     
 //*****************************************reading from .txt and storing data in array**********************//
 
-    fp = fopen("bram_a.txt","r");
-    if (NULL == fp) {
+    textfile = fopen("bram_a.txt","r");
+    if (textfile == NULL) {
         printf("File can't be opened \n");
     }
-    size = ftell(fp);
-    buffer_bram_a = malloc((size+1) * sizeof(*buffer_bram_a));
-    fread(buffer_bram_a, size, 1, fp);
-    buffer[size] = '\0';
-	printf("%s\n", buffer);	
+
+	fseek(textfile, 0L, SEEK_END);
+	numbytes = ftell(textfile);
+	fseek(textfile, 0L, SEEK_SET);
+
+	buffer_bram_a = (char*)calloc(numbytes, sizeof(char));
+	if(buffer_bram_a == NULL){
+		printf("buffer_bram_a == NULL \n");
+	}
+	
+	fread(buffer_bram_a, sizeof(char), numbytes, textfile);
+	fclose(textfile);
+	printf(buffer_bram_a);
 
 //**************************************************************************************************************//
-
+/*
 
 fp = fopen("/dev/imdct", "w");
 	
@@ -47,9 +56,9 @@ fp = fopen("/dev/imdct", "w");
 		printf("Problem pri zatvaranju /dev/imdct.\n");
 		return -1;	
     }
+*/
 
 
 
-
-
+	return 0;
 }
