46a96af436b59f9a5ff93a7738b696afc393e2ca
radicdjolee <114096761+radicdjolee@users.noreply.github.com>
2023-02-13 14:50:16 +0100

    Add files via upload

--- application.c
+++ application.c
@@ -7,13 +7,13 @@
 int main(void){
 
     FILE *fp;
-    char *text_bram_a;
+    char *text_bram_a = NULL;
     char *text_bram_b;
 	int  *bram_a_txt_array, *bram_b_txt_array;
-    long numbytes;
+    size_t size = 0;
 	//char *text = "2,2,2,2,2,2,2,2,2,2,2,";
     
-//**********************reading from bram_a.txt and storing data in array**********************//
+//**********************reading from bram_a.txt and storing data in array**********************
 
     fp = fopen("bram_a.txt","r");
     if (fp == NULL) {
@@ -21,28 +21,28 @@ int main(void){
     }
 
 	fseek(fp, 0L, SEEK_END);
-	numbytes = ftell(fp);
+	size = ftell(fp);
+	printf("velicina %d\n", size);
 	fseek(fp, 0L, SEEK_SET);
 
-	text_bram_a = (char*)calloc(numbytes, sizeof(char));
-	if(text_bram_a == NULL){
-		printf("text_bram_a == NULL \n");
-	}
-	
-	fread(text_bram_a, sizeof(char), numbytes, fp);
+	text_bram_a = malloc((size+1) * sizeof(*text_bram_a));
+
+	fread(text_bram_a, size, 1, fp);
+	text_bram_a[size] = '\0';
+
 	if(fclose(fp)){
 		printf("Problem pri zatvaranju text_file bram_a.txt\n");
 		return -1;	
 	}
-	sleep(0.5);
+	
 	printf("********************************\n");
 	printf("*************BRAM_A*************\n");
 	//printf(text_bram_a);
 	//printf("\n");
 	printf("********************************\n");
 
-//**********************reading from bram_b.txt and storing data in array**********************//
-
+//**********************reading from bram_b.txt and storing data in array**********************
+/*
     fp = fopen("bram_b.txt","r");
     if (fp == NULL) {
         printf("File can't be opened \n");
@@ -58,7 +58,9 @@ int main(void){
 	}
 	
 	fread(text_bram_b, sizeof(char), numbytes, fp);
-
+	
+	text_bram_b[sizeof(char)] = '\0';
+	
 	if(fclose(fp)){
 		printf("Problem pri zatvaranju text_file bram_b.txt\n");
 		return -1;	
@@ -70,7 +72,7 @@ int main(void){
 	//printf("\n");
 	printf("********************************\n");
 
-//***************************UPISUJEMO U /dev/bram_1***************************//
+//***************************UPISUJEMO U /dev/bram_1***************************
 
 	fp = fopen("/dev/bram_a", "w");
 	
@@ -81,13 +83,14 @@ int main(void){
 
 	fputs(text_bram_a,fp);
 
+
 	if(fclose(fp)){	
 		printf("Problem pri zatvaranju /dev/bram_a.\n");
 		return -1;	
 	}
 
 
-//*************************************************************************************************************//
+//*************************************************************************************************************
 
 fp = fopen("/dev/imdct", "w");
 	
@@ -213,6 +216,6 @@ fp = fopen("/dev/imdct", "w");
 		return -1;	
     }
 
-
+*/
 	return 0;
 }
